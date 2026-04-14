b81a8b8323b2e55507dffd7509c3981fd6c41404
muny-rith <munyrith.rupp@gmail.com>
2026-04-14 18:37:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    Plugin: [react()],
-    base: process.env.VITE_BASE_PATH || "/inventory-sm-react-js",
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/inventory-sm-react-js",
 })
