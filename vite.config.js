3890850c5fb95e0fafc10024c41085f61944d6ae
មុនី ឫទ្ធិ <myidea1311@gmail.com>
2026-04-14 18:45:57 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import {defineConfig} from 'vite'
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    Plugin: [react()],
+  plugins: [react()],
     base: process.env.VITE_BASE_PATH || "/inventory-sm-react-js",
+
 })
