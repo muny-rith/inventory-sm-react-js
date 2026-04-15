ad34946e0bd96c7232a7dee889eea72a779051a4
មុនី ឫទ្ធិ <myidea1311@gmail.com>
2026-04-15 17:34:30 +0700

    fix

--- src/pages/report/report.js
+++ src/pages/report/report.js
@@ -25,32 +25,6 @@ import LoanoutChart from '../../components/LoanoutChart/LoanoutChart';
 
 
 
-const rows = [
-  {
-    id: 1,
-    product: 'Organic Cream',
-    code: 'CREM01',
-    category: 'Beauty',
-    price: 25,
-    brand: 'Lakme',
-    cost: 10,
-    qty: 10,
-    image: 'https://via.placeholder.com/40',
-  },
-  {
-    id: 2,
-    product: 'Rain Umbrella',
-    code: 'UM01',
-    category: 'Grocery',
-    price: 30,
-    brand: 'Sun',
-    cost: 20,
-    qty: 15,
-    image: 'https://via.placeholder.com/40',
-  },
-];
-
-
 const Report = () => {
   return (
     <div className='container-fluid'>
