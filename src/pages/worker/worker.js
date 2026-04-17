cac0e720bd3af1bdeb7ae0b97df82cca8ffe4880
មុនី ឫទ្ធិ <myidea1311@gmail.com>
2026-04-17 16:04:07 +0700

    fix

--- src/pages/worker/worker.js
+++ src/pages/worker/worker.js
@@ -4,7 +4,7 @@ import DataTable from '../../components/DataTable/DataTable';
 import Button from '../../components/Form/Button';
 import Input from '../../components/Form/Input'
 
-import { Box, Avatar } from '@mui/material';
+import { Box} from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
